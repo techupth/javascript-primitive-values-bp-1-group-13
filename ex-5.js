0a2dfffe2d8541b7097244c44c84cb9381f22194
Supakit <book0970403815@hotmail.com>
2023-09-14 23:36:07 +0700

    feat : book do assignment ex1,2,3,4,5,6,7,8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
-// Start coding here
+let totalPrice = 27000
+let discount = 25 * totalPrice / 100
+console.log(discount)
+let truePrice = totalPrice - discount
+console.log(truePrice)
+truePrice = 20250
