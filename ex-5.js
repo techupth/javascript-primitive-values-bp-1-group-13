a04941ff8bbcc90c01eaad801d778b889b482437
GaiKT <124340777+GaiKT@users.noreply.github.com>
2023-09-15 01:16:40 +0700

    fix(ex1-8) : sand assignment by gai

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let productPrise = 27000;
+let discount = (productPrise*25)/100 ;
+let promotionProductPrise = productPrise - discount ;
+
+console.log(promotionProductPrise)
